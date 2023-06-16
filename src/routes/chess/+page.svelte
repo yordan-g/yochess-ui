4c1c24da618d955ecb2537d9761fe0c523027694
Yordan Ganev <yordan.ganev@ovo.com>
2023-06-16 18:18:40 +0300

    add pieces

--- src/routes/chess/+page.svelte
+++ src/routes/chess/+page.svelte
@@ -1,12 +1,37 @@
 <script>
     import "cm-chessboard/assets/chessboard.css";
-    import {Chessboard} from "cm-chessboard/src/Chessboard.js";
+    import "cm-chessboard/assets/extensions/markers/markers.css";
+    import {Chessboard, FEN, INPUT_EVENT_TYPE} from "cm-chessboard/src/Chessboard.js";
     import {onMount} from "svelte";
+    import {MARKER_TYPE, Markers} from "cm-chessboard/src/extensions/markers/Markers.js";
+
+    var board;
 
     onMount(async () => {
-        new Chessboard(document.getElementById("containerId"),
-            {position: "rn2k1r1/ppp1pp1p/3p2p1/5bn1/P7/2N2B2/1PPPPP2/2BNK1RR"})
+        board = new Chessboard(document.getElementById("containerId"), {
+            position: FEN.start,
+            assetsUrl: "../assets/",
+            extensions: [{class: Markers}]
+        })
+
+        board.enableMoveInput(inputHandler)
     });
+
+    function inputHandler(event) {
+        console.log(event)
+        board.removeMarkers(MARKER_TYPE.frame)
+        switch (event.type) {
+            case INPUT_EVENT_TYPE.moveInputStarted:
+                // log(`moveInputStarted: ${event.square}`)
+                return true
+            case INPUT_EVENT_TYPE.validateMoveInput:
+                // log(`validateMoveInput: ${event.squareFrom}-${event.squareTo}`)
+                return true
+            case INPUT_EVENT_TYPE.moveInputCanceled:
+            // log(`moveInputCanceled`)
+        }
+    }
+
 </script>
 
 <div class="board" id="containerId"></div>
